d51a4df7f32908e7dbe51bd483b32b04e06136a2
Diego Freniche Brito <diego@freniche.com>
2014-07-02 10:54:17 +0200

    Initial Commit

---
+++ MarvelAPIDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MarvelAPIDemo
+//
+//  Created by Diego Freniche Brito on 02/07/14.
+//  Copyright (c) 2014 Diego Freniche Brito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MarvelAPIDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MarvelAPIDemo
+//
+//  Created by Diego Freniche Brito on 02/07/14.
+//  Copyright (c) 2014 Diego Freniche Brito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
